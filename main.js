373affca6d7722eb87c32ab5e47544310d52ed75
leesol <jbkljo@naver.com>
2022-06-28 17:01:21 +0900

    Create project

---
+++ main.js
@@ -0,0 +1,11 @@
+let str =`
+010-1234-1234
+1234qwer@gmail.com
+https://www.omdbapi.com/?apikey=7035c60c&s=frozen
+The quick brown fox jumps over the lazy dog.
+abbcccdddd
+`
+
+console.log(
+  str.match(/./g)
+  )
